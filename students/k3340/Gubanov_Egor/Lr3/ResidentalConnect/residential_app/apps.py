88cff73317b1d0734836c33d54af3bdfd74158ff
Egor <egorr.gubanov@mail.ru>
2025-11-24 01:27:40 +0300

    Add Django app configuration files

---
+++ students/k3340/Gubanov_Egor/Lr3/ResidentalConnect/residential_app/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ResidentialAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'residential_app'
+    verbose_name = 'ЖК Коннект'
+
